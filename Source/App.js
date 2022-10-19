af2aa4843a2032f3f74ab06e50bca24b8071a9a1
ALewdDev <ALewdDev@gmail.com>
2022-10-19 17:57:55 -0400

    🐾 Added encoding imports

--- Source/App.js
+++ Source/App.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env -S deno run --importmap=Source/ImportMap.json
 
 
+import { parse as parseYAML } from 'YAML'
+import { parse as parseINI } from 'INI'
+
+
 const { clear , log } = console;
 
 
 clear();
 
 log('Hardening the Fiery Fox')
-
