8f38a417f90e187ecfd64f47aa1160782daf876b
ALewdDev <ALewdDev@gmail.com>
2022-10-19 20:14:49 -0400

    🐾 Defined theme colors

--- Source/App.js
+++ Source/App.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env -S deno run --allow-env=HOME --allow-read=Source/Options.yaml,${HOME}/.mozilla/firefox/profiles.ini --importmap=Source/ImportMap.json
 
 
+import { Background , AccentA , AccentB , Font } from './Colors.js'
+import { bgRgb24 , rgb24 } from 'Color'
+import loadProfiles from './Profiles.js'
 import * as Paths from './Paths.js'
 import { parse } from 'YAML'
-import loadProfiles from './Profiles.js'
 
 
 const { clear , log } = console;
---
+++ Source/Colors.js
@@ -0,0 +1,13 @@
+
+
+export const Background
+    = 0x1f1f1f ;
+
+export const AccentA
+    = 0xf9768e ;
+
+export const AccentB
+    = 0xc17ff4 ;
+    
+export const Font
+    = 0xcbcbcb ;
